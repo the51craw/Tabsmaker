ea3a484e5e3fa910a0f3936af8f67d58603034a0
the51craw <35884608+the51craw@users.noreply.github.com>
2018-11-18 21:06:33 +0100

    First commit.

---
+++ Tabsmaker/Tabsmaker.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Tabsmaker
+{
+    partial class Tabsmaker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tabsmaker));
+            this.pbSystem = new System.Windows.Forms.PictureBox();
+            this.lblNote = new System.Windows.Forms.Label();
+            this.lblTab1 = new System.Windows.Forms.Label();
+            this.pbTab1 = new System.Windows.Forms.PictureBox();
+            this.lblTab2 = new System.Windows.Forms.Label();
+            this.pbTab2 = new System.Windows.Forms.PictureBox();
+            this.lblTab3 = new System.Windows.Forms.Label();
+            this.pbTab3 = new System.Windows.Forms.PictureBox();
+            this.lblTab4 = new System.Windows.Forms.Label();
+            this.pbTab4 = new System.Windows.Forms.PictureBox();
+            this.lblTab5 = new System.Windows.Forms.Label();
+            this.pbTab5 = new System.Windows.Forms.PictureBox();
+            this.lblTab6 = new System.Windows.Forms.Label();
+            this.pbTab6 = new System.Windows.Forms.PictureBox();
+            this.lblTab7 = new System.Windows.Forms.Label();
+            this.pbTab7 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSystem)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTab1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTab2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTab3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTab4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTab5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTab6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTab7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbSystem
+            // 
+            this.pbSystem.Image = ((System.Drawing.Image)(resources.GetObject("pbSystem.Image")));
+            this.pbSystem.Location = new System.Drawing.Point(12, 12);
+            this.pbSystem.Name = "pbSystem";
+            this.pbSystem.Size = new System.Drawing.Size(954, 233);
+            this.pbSystem.TabIndex = 0;
+            this.pbSystem.TabStop = false;
+            this.pbSystem.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbSystem_MouseDown);
+            this.pbSystem.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbSystem_MouseUp);
+            // 
+            // lblNote
+            // 
+            this.lblNote.AutoSize = true;
+            this.lblNote.Location = new System.Drawing.Point(148, 38);
+            this.lblNote.Name = "lblNote";
+            this.lblNote.Size = new System.Drawing.Size(0, 13);
+            this.lblNote.TabIndex = 4;
+            // 
+            // lblTab1
+            // 
+            this.lblTab1.AutoSize = true;
+            this.lblTab1.Location = new System.Drawing.Point(31, 250);
+            this.lblTab1.Name = "lblTab1";
+            this.lblTab1.Size = new System.Drawing.Size(10, 13);
+            this.lblTab1.TabIndex = 7;
+            this.lblTab1.Text = "I";
+            // 
+            // pbTab1
+            // 
+            this.pbTab1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbTab1.BackgroundImage")));
+            this.pbTab1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbTab1.Location = new System.Drawing.Point(14, 271);
+            this.pbTab1.Name = "pbTab1";
+            this.pbTab1.Size = new System.Drawing.Size(115, 249);
+            this.pbTab1.TabIndex = 6;
+            this.pbTab1.TabStop = false;
+            // 
+            // lblTab2
+            // 
+            this.lblTab2.AutoSize = true;
+            this.lblTab2.Location = new System.Drawing.Point(170, 250);
+            this.lblTab2.Name = "lblTab2";
+            this.lblTab2.Size = new System.Drawing.Size(10, 13);
+            this.lblTab2.TabIndex = 9;
+            this.lblTab2.Text = "I";
+            // 
+            // pbTab2
+            // 
+            this.pbTab2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbTab2.BackgroundImage")));
+            this.pbTab2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbTab2.Location = new System.Drawing.Point(153, 271);
+            this.pbTab2.Name = "pbTab2";
+            this.pbTab2.Size = new System.Drawing.Size(115, 249);
+            this.pbTab2.TabIndex = 8;
+            this.pbTab2.TabStop = false;
+            // 
+            // lblTab3
+            // 
+            this.lblTab3.AutoSize = true;
+            this.lblTab3.Location = new System.Drawing.Point(309, 250);
+            this.lblTab3.Name = "lblTab3";
+            this.lblTab3.Size = new System.Drawing.Size(10, 13);
+            this.lblTab3.TabIndex = 11;
+            this.lblTab3.Text = "I";
+            // 
+            // pbTab3
+            // 
+            this.pbTab3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbTab3.BackgroundImage")));
+            this.pbTab3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbTab3.Location = new System.Drawing.Point(292, 271);
+            this.pbTab3.Name = "pbTab3";
+            this.pbTab3.Size = new System.Drawing.Size(115, 249);
+            this.pbTab3.TabIndex = 10;
+            this.pbTab3.TabStop = false;
+            // 
+            // lblTab4
+            // 
+            this.lblTab4.AutoSize = true;
+            this.lblTab4.Location = new System.Drawing.Point(448, 250);
+            this.lblTab4.Name = "lblTab4";
+            this.lblTab4.Size = new System.Drawing.Size(10, 13);
+            this.lblTab4.TabIndex = 13;
+            this.lblTab4.Text = "I";
+            // 
+            // pbTab4
+            // 
+            this.pbTab4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbTab4.BackgroundImage")));
+            this.pbTab4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbTab4.Location = new System.Drawing.Point(431, 271);
+            this.pbTab4.Name = "pbTab4";
+            this.pbTab4.Size = new System.Drawing.Size(115, 249);
+            this.pbTab4.TabIndex = 12;
+            this.pbTab4.TabStop = false;
+            // 
+            // lblTab5
+            // 
+            this.lblTab5.AutoSize = true;
+            this.lblTab5.Location = new System.Drawing.Point(587, 250);
+            this.lblTab5.Name = "lblTab5";
+            this.lblTab5.Size = new System.Drawing.Size(10, 13);
+            this.lblTab5.TabIndex = 15;
+            this.lblTab5.Text = "I";
+            // 
+            // pbTab5
+            // 
+            this.pbTab5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbTab5.BackgroundImage")));
+            this.pbTab5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbTab5.Location = new System.Drawing.Point(570, 271);
+            this.pbTab5.Name = "pbTab5";
+            this.pbTab5.Size = new System.Drawing.Size(115, 249);
+            this.pbTab5.TabIndex = 14;
+            this.pbTab5.TabStop = false;
+            // 
+            // lblTab6
+            // 
+            this.lblTab6.AutoSize = true;
+            this.lblTab6.Location = new System.Drawing.Point(726, 250);
+            this.lblTab6.Name = "lblTab6";
+            this.lblTab6.Size = new System.Drawing.Size(10, 13);
+            this.lblTab6.TabIndex = 17;
+            this.lblTab6.Text = "I";
+            // 
+            // pbTab6
+            // 
+            this.pbTab6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbTab6.BackgroundImage")));
+            this.pbTab6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbTab6.Location = new System.Drawing.Point(709, 271);
+            this.pbTab6.Name = "pbTab6";
+            this.pbTab6.Size = new System.Drawing.Size(115, 249);
+            this.pbTab6.TabIndex = 16;
+            this.pbTab6.TabStop = false;
+            // 
+            // lblTab7
+            // 
+            this.lblTab7.AutoSize = true;
+            this.lblTab7.Location = new System.Drawing.Point(865, 250);
+            this.lblTab7.Name = "lblTab7";
+            this.lblTab7.Size = new System.Drawing.Size(10, 13);
+            this.lblTab7.TabIndex = 19;
+            this.lblTab7.Text = "I";
+            // 
+            // pbTab7
+            // 
+            this.pbTab7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbTab7.BackgroundImage")));
+            this.pbTab7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbTab7.Location = new System.Drawing.Point(848, 271);
+            this.pbTab7.Name = "pbTab7";
+            this.pbTab7.Size = new System.Drawing.Size(115, 249);
+            this.pbTab7.TabIndex = 18;
+            this.pbTab7.TabStop = false;
+            // 
+            // Tabsmaker
+            // 
+            this.ClientSize = new System.Drawing.Size(986, 533);
+            this.Controls.Add(this.lblTab7);
+            this.Controls.Add(this.pbTab7);
+            this.Controls.Add(this.lblTab6);
+            this.Controls.Add(this.pbTab6);
+            this.Controls.Add(this.lblTab5);
+            this.Controls.Add(this.pbTab5);
+            this.Controls.Add(this.lblTab4);
+            this.Controls.Add(this.pbTab4);
+            this.Controls.Add(this.lblTab3);
+            this.Controls.Add(this.pbTab3);
+            this.Controls.Add(this.lblTab2);
+            this.Controls.Add(this.pbTab2);
+            this.Controls.Add(this.lblTab1);
+            this.Controls.Add(this.pbTab1);
+            this.Controls.Add(this.lblNote);
+            this.Controls.Add(this.pbSystem);
+            this.Name = "Tabsmaker";
+            this.Text = "Tabs maker";
+            ((System.ComponentModel.ISupportInitialize)(this.pbSystem)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTab1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTab2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTab3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTab4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTab5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTab6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTab7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbSystem;
+        private System.Windows.Forms.Label lblNote;
+        private System.Windows.Forms.Label lblTab1;
+        private System.Windows.Forms.PictureBox pbTab1;
+        private System.Windows.Forms.Label lblTab2;
+        private System.Windows.Forms.PictureBox pbTab2;
+        private System.Windows.Forms.Label lblTab3;
+        private System.Windows.Forms.PictureBox pbTab3;
+        private System.Windows.Forms.Label lblTab4;
+        private System.Windows.Forms.PictureBox pbTab4;
+        private System.Windows.Forms.Label lblTab5;
+        private System.Windows.Forms.PictureBox pbTab5;
+        private System.Windows.Forms.Label lblTab6;
+        private System.Windows.Forms.PictureBox pbTab6;
+        private System.Windows.Forms.Label lblTab7;
+        private System.Windows.Forms.PictureBox pbTab7;
+    }
+}
+
---
+++ Tabsmaker/Tabsmaker.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabsmaker
+{
+
+    public partial class Tabsmaker : Form
+    {
+        Boolean[] notes = new Boolean[22]; // Corresponds to drawn notes and hit-areas regardless of # or b.
+        Boolean[] augmentedNotes = new Boolean[37]; // Corresponds to real notes including # and b.
+        Int32 augmentation = 0;
+        Int32 notesIndex;
+        Int32 augmentedNotesIndex;
+        Int32[] augmentationtranslation = new int[] { 0, 1, 3, 5, 7, 8, 10, 12, 13, 15, 17, 19, 20, 22, 24, 25, 27, 29, 31, 32, 34, 36 };
+        // e  f  g  a  h  c  d   e   f   g   a   h   c   d   e   f   g   a   h   c   d   e
+        String[] noteNames = new string[] { "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4",
+                                            "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5", "C#5", "D5", "D#5",
+                                            "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5", "C6", "C#6", "D6", "D#6", "E6"};
+        Rectangle[] rectangles = new Rectangle[22];
+        List<Position> Positions = new List<Position>();
+        Int32 downClickedPosition;
+        //Int32[]
+
+        public Tabsmaker()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            Int32 x = 76;
+            Int32 y = 177;
+            Boolean odd = false;
+            for (Int32 i = 0; i < 22; i++)
+            {
+                rectangles[i] = new Rectangle(x, y, 39, 26);
+                x += 40;
+                if (odd)
+                {
+                    y -= 8;
+                    odd = false;
+                }
+                else
+                {
+                    y -= 9;
+                    odd = true;
+                }
+            }
+        }
+
+        private void btnUpNeck_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDownNeck_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbSystem_MouseDown(object sender, MouseEventArgs e)
+        {
+            downClickedPosition = e.Y;
+            notesIndex = CheckForHit(e.X, e.Y);
+            if (notesIndex > -1)
+            {
+                // You may only add 4 fingers!
+                if (!notes[notesIndex] && GetFingerCount() >= 4)
+                {
+                    MessageBox.Show(this, "You may only add 4 notes!", "Tabs maker: Error!");
+                }
+                else
+                {
+                    notes[notesIndex] = !notes[notesIndex];
+                    DrawNote(notesIndex);
+                }
+            }
+        }
+
+        private void pbSystem_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (downClickedPosition > e.Y + 8)
+            {
+                augmentation = +1;
+            }
+            else if (downClickedPosition < e.Y - 8)
+            {
+                augmentation = -1;
+            }
+            else
+            {
+                augmentation = 0;
+            }
+            augmentedNotesIndex = augmentationtranslation[notesIndex] + augmentation;
+            // Do not allow b before lowest E nor # before highest E:
+            if (augmentedNotesIndex < 0)
+            {
+                augmentedNotesIndex = 0;
+            }
+            else if (augmentedNotesIndex > 36)
+            {
+                augmentedNotesIndex = 36;
+            }
+            lblNote.Text = noteNames[augmentedNotesIndex];
+            UpdateFingerPositions();
+        }
+
+        private Int32 GetFingerCount()
+        {
+            Int32 count = 0;
+            for (Int32 i = 20; i > -1; i--)
+            {
+                if (notes[i])
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private void UpdateFingerPositions()
+        {
+            Positions.Clear();
+            Boolean first = true;
+            //Int32 stringNumber = 0; // 0 = thinnest string!
+
+            // Start from top since using one finger position inhibits
+            // using the same string for more finger positions.
+            // Iterate all notes:
+            for (Int32 i = 35; i > -1; i--)
+            {
+                //if (augmentedNotes[i])
+                {
+                    // Find all possible positions for this note.
+                    Int32 pos = 36;
+
+                    while (pos > -1)
+                    {
+                        if (noteNames[pos].StartsWith(lblNote.Text.Remove(lblNote.Text.Length - 1, 1))) // Compare label to list disregarding octave number
+                        {
+                            // If this is the first note found, create lists of Position
+                            // elemets to put the notes in:
+                            if (first)
+                            {
+                                Positions.Add(new Position());
+                            }
+
+                            augmentedNotes[pos] = true;
+
+                            if (pos >= 24)
+                            {
+                                Positions[Positions.Count - 1].FingerPositions[0].String = 0;
+                                Positions[Positions.Count - 1].FingerPositions[0].Fret = pos - 24;
+                            }
+                            else if (pos >= 19)
+                            {
+                                Positions[Positions.Count - 1].FingerPositions[1].String = 1;
+                                Positions[Positions.Count - 1].FingerPositions[1].Fret = pos - 19;
+                            }
+                            else if (pos >= 15)
+                            {
+                                Positions[Positions.Count - 1].FingerPositions[2].String = 2;
+                                Positions[Positions.Count - 1].FingerPositions[2].Fret = pos - 15;
+                            }
+                            else if (pos >= 10)
+                            {
+                                Positions[Positions.Count - 1].FingerPositions[3].String = 3;
+                                Positions[Positions.Count - 1].FingerPositions[3].Fret = pos - 10;
+                            }
+                            else if (pos >= 5)
+                            {
+                                Positions[Positions.Count - 1].FingerPositions[4].String = 4;
+                                Positions[Positions.Count - 1].FingerPositions[4].Fret = pos - 5;
+                            }
+                            else
+                            {
+                                Positions[Positions.Count - 1].FingerPositions[5].String = 5;
+                                Positions[Positions.Count - 1].FingerPositions[5].Fret = pos;
+                            }
+
+                        }
+                        pos--;
+
+
+                        //for (Int32 pos = 0; pos < 4 && Positions[Positions.Count - 1].FingerPositions[pos] != null; pos++)
+                        //{
+
+                        //}
+                        //Positions[Positions.Count - 1];
+                    }
+
+                    if (first)
+                    {
+                        first = false;
+                    }
+
+                    // Find all positions for this note on the fretboard:
+                    // create Positions objects for them and fill out first
+                }
+            }
+        }
+
+        private Int32 CheckForHit(Int32 x, Int32 y)
+        {
+            for (Int32 i = 0; i < 22; i++)
+            {
+                if (x >= rectangles[i].Left
+                    && x <= rectangles[i].Right
+                    && y >= rectangles[i].Top
+                    && y <= rectangles[i].Bottom)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void DrawNotes()
+        {
+            for (Int32 i = 0; i < 22; i++)
+            {
+                DrawNote(i);
+            }
+        }
+
+        private void DrawNote(Int32 note)
+        {
+            Graphics g = pbSystem.CreateGraphics();
+            if (notes[note])
+            {
+                g.FillEllipse(Brushes.Green, rectangles[note].Left + 6, rectangles[note].Top + 4, 23, 14);
+            }
+            else
+            {
+                g.FillEllipse(Brushes.Black, rectangles[note].Left + 6, rectangles[note].Top + 4, 23, 14);
+            }
+        }
+    }
+
+    /* Class position holds one possible combination of 
+     * finger positions for the selected notes. Make a list
+     * of Position objects to select from.
+     */
+    public class Position
+    {
+        public FingerPosition[] FingerPositions { get; set; }
+
+        public Position()
+        {
+            FingerPositions = new FingerPosition[7];
+            for (Int32 i = 0; i < 7; i++)
+            {
+                FingerPositions[i] = new FingerPosition();
+            }
+        }
+    }
+
+    /* Class FingerPosition holds one possible finger position
+     * for one single note. Use in class Position.
+     */
+    public class FingerPosition
+    {
+        public Int32 String { get; set; }
+        public Int32 Fret { get; set; }
+
+        public FingerPosition()
+        {
+            String = -1; // Indicates not in use!
+            Fret = -1;   // Indicates not in use!
+        }
+    }
+
+    /* Class GuitarString holds a list of note numbers for one string.
+     * Make 6 GuitarString objects and use to find notes.
+     */
+    public class GuitarString
+    {
+        public List<Int32> Frets { get; set; }
+
+        public GuitarString(Int32 stringNumber, Int32 note)
+        {
+            Int32 lowest = 0;
+            switch (stringNumber)
+            {
+                case 0:
+                    lowest = 14;
+                    break;
+                case 1:
+                    lowest = 11;
+                    break;
+                case 2:
+                    lowest = 9;
+                    break;
+                case 3:
+                    lowest = 6;
+                    break;
+                case 4:
+                    lowest = 3;
+                    break;
+                case 5:
+                    lowest = 0;
+                    break;
+            }
+        }
+    }
+}
